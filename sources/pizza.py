ee175204e84530a60bdd62b3be70771bbf9cef07
Steeve Asensi <asensi.steeve@gmail.com>
2022-01-08 17:39:48 +0100

    Add New Pizza

--- sources/pizza.py
+++ sources/pizza.py
@@ -35,5 +35,9 @@ RECIPES = {
     'mediterranea': {
         'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
         'price': 11,
-    }
+    },
+    'my pizza perso': {
+        'ingredients': {'gorgonzola': 3, 'mozzarella': 4, 'onion': 3, 'pepper': 4},
+        'price': 10,
+    },
 }
